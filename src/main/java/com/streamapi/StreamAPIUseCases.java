5d92115ae2d7cb77d68a99204080c1f556054abe
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-03-03 14:59:20 +0530

    UC2.1 forEach

---
+++ src/main/java/com/streamapi/StreamAPIUseCases.java
@@ -0,0 +1,7 @@
+package com.streamapi;
+import java.util.*;
+public class StreamAPIUseCases {
+ public static void main(String[] args){
+  Arrays.asList(1,2,3,4,5).stream().forEach(System.out::println);
+ }
+}
