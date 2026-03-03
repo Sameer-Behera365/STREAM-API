cb8567b0b3e06f5fee69584e57712f38d8a0e4c7
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-03-03 15:44:04 +0530

    UC2.9 sorted

---
+++ src/main/java/com/streamapi/StreamAPIUseCases.java
@@ -0,0 +1,12 @@
+package com.streamapi;
+import java.util.*;
+import java.util.stream.*;
+public class StreamAPIUseCases {
+ public static void main(String[] args){
+  System.out.println(
+   Arrays.asList(9,3,7,1,5)
+   .stream().sorted()
+   .collect(Collectors.toList())
+  );
+ }
+}
